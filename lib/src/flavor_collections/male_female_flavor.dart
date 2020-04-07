6e0984fd514a2427d20d2f319dc322a8edaff714
giorgiofran <giorgiofran@gmail.com>
2020-04-07 17:17:59 +0200

    shortcuts for flavor collections

--- lib/src/flavor_collections/male_female_flavor.dart
+++ lib/src/flavor_collections/male_female_flavor.dart
@@ -1,5 +1,7 @@
 import 'package:vy_dart_meme/src/element/flavor_collection.dart';
 
+const MaleFemaleFlavor maleFemaleFlavor = MaleFemaleFlavor();
+
 class MaleFemaleFlavor extends FlavorCollection {
   static const male = 'male';
   static const female = 'female';
--- lib/src/flavor_collections/plural_flavor.dart
+++ lib/src/flavor_collections/plural_flavor.dart
@@ -1,5 +1,7 @@
 import 'package:vy_dart_meme/src/element/flavor_collection.dart';
 
+const PluralFlavor pluralFlavor = PluralFlavor();
+
 class PluralFlavor extends FlavorCollection {
   static const singular = 'singular';
   static const plural = 'plural';
